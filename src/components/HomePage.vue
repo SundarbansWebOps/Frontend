517f6e3fac5cd2e5368772b1461012dc5f2b940c
ravi <rvk303804@gmail.com>
2024-01-10 16:31:27 +0530

    Dynamic rendering added for Homepage button

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,12 @@
 <template>
   <NavBar></NavBar>
+
   <body class="dark:bg-black">
     <!-- hero section -->
     <section class="bg-white dark:bg-black">
       <div class="container px-6 py-16 mx-auto text-center">
         <div class="max-w-lg mx-auto">
-          <h1
-            class="text-3xl font-semibold text-gray-800 dark:text-[#eab308] lg:text-4xl"
-          >
+          <h1 class="text-3xl font-semibold text-gray-800 dark:text-[#eab308] lg:text-4xl">
             Welcome to Sundarbans House, IIT Madras!
           </h1>
           <p class="mt-6 text-gray-500 dark:text-gray-300">
@@ -16,21 +15,22 @@
             dedicated house council for peer support, driving positive change
             and holistic student development.
           </p>
-          <a href="https://forms.gle/7pwqYUXWLS7pLpfo8"
-            ><button
-              class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-black capitalize dark:bg-white bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none"
-            >
-              Participate in Sangeet Sangam
-            </button></a
-          >
+          <router-link v-if="upcomingEvents && upcomingEvents.length > 0"
+            :to="'/event/' + (upcomingEvents[0].slug || '')">
+            <button
+              class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-black capitalize dark:bg-white bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
+              Participate in {{ upcomingEvents[0].title }}
+            </button>
+          </router-link>
+
+
+
           <!-- <p class="mt-3 text-sm text-gray-400 ">No credit card required</p> -->
         </div>
 
         <div class="flex justify-center mt-10">
-          <img
-            class="object-cover w-full h-96 rounded-xl lg:w-4/5"
-            src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-          />
+          <img class="object-cover w-full h-96 rounded-xl lg:w-4/5"
+            src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" />
         </div>
       </div>
     </section>
@@ -39,9 +39,7 @@
     <section class="bg-white dark:bg-black">
       <div class="container px-6 py-10 mx-auto">
         <div class="text-center">
-          <h1
-            class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white"
-          >
+          <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
             Upcoming Events
           </h1>
           <p class="max-w-lg mx-auto mt-4 text-gray-500">
@@ -51,31 +49,19 @@
 
         <div class="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
           <div v-for="event in upcomingEvents" :key="event.slug">
-            <img
-              class="relative z-10 object-cover w-full rounded-md h-96"
-              :src="prependBackendLink(event.image)"
-              alt="Event Image"
-            />
-            <div
-              class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-black"
-            >
-              <a
-                :href="event.form_url"
-                class="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl"
-              >
+            <img class="relative z-10 object-cover w-full rounded-md h-96" :src="prependBackendLink(event.image)"
+              alt="Event Image" />
+            <div class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-black">
+              <a :href="event.form_url" class="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
                 {{ event.title }}
               </a>
-              <p
-                class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm"
-              >
-              {{ truncateDescription(event.desc) }}
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                {{ truncateDescription(event.desc) }}
               </p>
               <p class="mt-3 text-sm text-[#eab308]">
-                {{ event.timestamp }} 
+                {{ event.timestamp }}
               </p>
-              <p
-                class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm"
-              >
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
                 Deadline: {{ event.deadline }}
               </p>
               <!-- <p
@@ -98,9 +84,7 @@
     <section class="bg-white py-10 dark:bg-black">
       <div class="container px-6 py-10 mx-auto">
         <div class="text-center">
-          <h1
-            class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white"
-          >
+          <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
             From Youtube
           </h1>
 
@@ -110,26 +94,16 @@
         </div>
 
         <div class="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
-          <div
-            class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-            style="padding-top: 56.25%"
-          >
-            <iframe
-              class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-              src="https://www.youtube.com/embed/6N-PfwyjsIQ?si=9TZ9-A541tzlhkCU"
-              allowfullscreen
-            ></iframe>
+          <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+            style="padding-top: 56.25%">
+            <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+              src="https://www.youtube.com/embed/6N-PfwyjsIQ?si=9TZ9-A541tzlhkCU" allowfullscreen></iframe>
           </div>
 
-          <div
-            class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-            style="padding-top: 56.25%"
-          >
-            <iframe
-              class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-              src="https://www.youtube.com/embed/RLBxlaK_aU4?si=NF0U6NkSsvkvSNHZ"
-              allowfullscreen
-            ></iframe>
+          <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+            style="padding-top: 56.25%">
+            <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+              src="https://www.youtube.com/embed/RLBxlaK_aU4?si=NF0U6NkSsvkvSNHZ" allowfullscreen></iframe>
           </div>
         </div>
       </div>
@@ -137,51 +111,31 @@
       <div class="container px-5 py-10 mx-auto">
         <div class="flex flex-wrap -m-4">
           <div class="p-4 md:w-1/2 lg:w-1/4 w-full">
-            <div
-              class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-              style="padding-top: 56.25%"
-            >
-              <iframe
-                class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                src="https://www.youtube.com/embed/TvkTrLJonbw?si=4k-pqv4Sm3IxT1J3"
-                allowfullscreen
-              ></iframe>
+            <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+              style="padding-top: 56.25%">
+              <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+                src="https://www.youtube.com/embed/TvkTrLJonbw?si=4k-pqv4Sm3IxT1J3" allowfullscreen></iframe>
             </div>
           </div>
           <div class="p-4 md:w-1/2 lg:w-1/4 w-full">
-            <div
-              class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-              style="padding-top: 56.25%"
-            >
-              <iframe
-                class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                src="https://www.youtube.com/embed/kEsry2TSVKU?si=AvUDyr9nmcz3wMjv"
-                allowfullscreen
-              ></iframe>
+            <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+              style="padding-top: 56.25%">
+              <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+                src="https://www.youtube.com/embed/kEsry2TSVKU?si=AvUDyr9nmcz3wMjv" allowfullscreen></iframe>
             </div>
           </div>
           <div class="p-4 md:w-1/2 lg:w-1/4 w-full">
-            <div
-              class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-              style="padding-top: 56.25%"
-            >
-              <iframe
-                class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                src="https://www.youtube.com/embed/QW4T_FQzi2I?si=AozEo2ZoQhIfZHas"
-                allowfullscreen
-              ></iframe>
+            <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+              style="padding-top: 56.25%">
+              <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+                src="https://www.youtube.com/embed/QW4T_FQzi2I?si=AozEo2ZoQhIfZHas" allowfullscreen></iframe>
             </div>
           </div>
           <div class="p-4 md:w-1/2 lg:w-1/4 w-full">
-            <div
-              class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
-              style="padding-top: 56.25%"
-            >
-              <iframe
-                class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
-                src="https://www.youtube.com/embed/rZdih019rz4?si=Bi5QHuM6YXaO0uTX"
-                allowfullscreen
-              ></iframe>
+            <div class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+              style="padding-top: 56.25%">
+              <iframe class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+                src="https://www.youtube.com/embed/rZdih019rz4?si=Bi5QHuM6YXaO0uTX" allowfullscreen></iframe>
             </div>
           </div>
         </div>
@@ -191,9 +145,7 @@
     <!-- latest events -->
     <section class="bg-white dark:bg-black">
       <div class="container px-6 py-10 mx-auto">
-        <h1
-          class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white"
-        >
+        <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">
           Latest Events
         </h1>
 
@@ -202,22 +154,12 @@
           latest happenings and exciting activities within our community.
         </p>
 
-        <div
-          class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3"
-        >
+        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
           <div v-for="event in latestEvents" :key="event.slug">
-            <img
-              class="relative z-10 object-cover w-full rounded-md h-96"
-              :src="prependBackendLink(event.image)"
-              alt="Event Image"
-            />
-            <div
-              class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-black"
-            >
-              <a
-                :href="event.form_url"
-                class="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl"
-              >
+            <img class="relative z-10 object-cover w-full rounded-md h-96" :src="prependBackendLink(event.image)"
+              alt="Event Image" />
+            <div class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-black">
+              <a :href="event.form_url" class="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
                 {{ event.title }}
               </a>
               <!-- <p
