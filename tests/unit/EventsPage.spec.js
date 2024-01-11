3f666df09a88b167bfeb1b40e1ce17e6b24071fd
Prajjwal <prajjwalyd@gmail.com>
2024-01-11 07:25:15 +0530

    basic unit tests added

--- tests/unit/EventsPage.spec.js
+++ tests/unit/EventsPage.spec.js
@@ -1,2 +1,9 @@
 import { mount } from '@vue/test-utils';
 import EventsPage from '../../src/components/EventsPage';
+
+describe('EventsPage.vue', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(EventsPage);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
--- tests/unit/HomePage.spec.js
+++ tests/unit/HomePage.spec.js
@@ -1,2 +1,9 @@
 import { mount } from '@vue/test-utils';
 import HomePage from '../../src/components/HomePage';
+
+describe('HomePage.vue', () => {
+  it('renders without errors', () => {
+    const wrapper = mount(HomePage);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
