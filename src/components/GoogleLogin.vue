ab9eae37379c330f62691e36cf20724987e62d5a
ravi <rvk303804@gmail.com>
2024-08-14 02:50:42 +0530

    login page image updated.

--- src/components/GoogleLogin.vue
+++ src/components/GoogleLogin.vue
@@ -7,7 +7,7 @@
       /> -->
       <div
         class="hidden bg-cover lg:block lg:w-1/2"
-        style="background-image: url('../assets/sundarbans2.png');"
+        style="background-image: url('https://images.unsplash.com/photo-1529156069898-49953e39b3ac?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"
       />
 
       <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
