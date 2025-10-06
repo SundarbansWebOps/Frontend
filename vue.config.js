87c06dcaef2c4ede21d151e415ddbc62013f755e
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2025-10-06 20:10:04 +0530

    Updated Node version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // transpileDependencies should be an array of dependency names to transpile.
+  // It was accidentally set to `true` which causes Vue CLI to call `.map` on it.
+  transpileDependencies: []
 })
