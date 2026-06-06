293d149b578dea0118d01fb9c381c5591c0ba7e6
Samar Ahmad <codewithsamsol@gmail.com>
2026-06-06 11:48:46 +0530

    Update leaderboard data with new points and entries

--- src/data/leaderboard.js
+++ src/data/leaderboard.js
@@ -1,26 +1,27 @@
 export const leaderboardData = {
   allTime: [
-    { name: 'Nivash Kumar',  city: 'Patna',      points: 1400, badge: 'Apex',     initial: 'N' },
+    { name: 'Nivash Kumar',  city: 'Patna',      points: 2000, badge: 'Apex',     initial: 'N' },
     { name: 'Rushabh Kapse', city: 'Mumbai',     points: 1300, badge: 'Zenith',   initial: 'R' },
-    { name: 'Divya Prakash', city: 'Delhi NCR',  points: 1000, badge: 'Pinnacle', initial: 'D' },
-    { name: 'Aakash Rawal',  city: 'Chandigarh', points: 800,  badge: 'Summit',   initial: 'A' },
-    { name: 'Chandan Saw',   city: 'Kolkata',    points: 500,  badge: 'Ascent',   initial: 'C' },
-    { name: 'Dishi Gupta',   city: 'Hyderabad',  points: 400,  badge: 'Rise',     initial: 'D' },
+    { name: 'Divya Prakash', city: 'Delhi NCR',  points: 1300, badge: 'Zenith', initial: 'D' },
+    { name: 'Aakash Rawal',  city: 'Chandigarh', points: 900,  badge: 'Pinnacle',   initial: 'A' },
+    { name: 'Chandan Saw',   city: 'Kolkata',    points: 500,  badge: 'Summit',   initial: 'C' },
+    { name: 'Dishi Gupta',   city: 'Hyderabad',  points: 400,  badge: 'Ascent',     initial: 'D' },
+    { name: 'Kartik Sharma', city: 'Chennai',    points: 300,  badge: 'Rise',   initial: 'K' },
     { name: 'R Sai Sahanaa', city: 'Chennai',    points: 100,  badge: 'Ignite',   initial: 'R' },
+    { name: 'Aditri B', city: 'Bangalore',    points: 100,  badge: 'Ignite',   initial: 'A' },
   ],
   term1: [
     { name: 'Divya Prakash', city: 'Delhi NCR',  points: 500, badge: 'Pinnacle', initial: 'D' },
-    { name: 'Nivash Kumar',  city: 'Patna',      points: 420, badge: 'Apex',     initial: 'N' },
+    { name: 'Nivash Kumar',  city: 'Patna',      points: 400, badge: 'Apex',     initial: 'N' },
     { name: 'Chandan Saw',   city: 'Kolkata',    points: 300, badge: 'Ascent',   initial: 'C' },
   ],
   term2: [
     { name: 'Rushabh Kapse', city: 'Mumbai',     points: 700, badge: 'Zenith',   initial: 'R' },
     { name: 'Nivash Kumar',  city: 'Patna',      points: 600, badge: 'Apex',     initial: 'N' },
     { name: 'Aakash Rawal',  city: 'Chandigarh', points: 400, badge: 'Summit',   initial: 'A' },
-    { name: 'Dishi Gupta',   city: 'Hyderabad',  points: 200, badge: 'Rise',     initial: 'D' },
-  ],
+    
   term3: [
-    { name: 'Rushabh Kapse', city: 'Mumbai',     points: 600, badge: 'Zenith',   initial: 'R' },
+    { name: 'Nivash', city: 'Patna',     points: 1000, badge: 'Apex',   initial: 'N' },
     { name: 'R Sai Sahanaa', city: 'Chennai',    points: 100, badge: 'Ignite',   initial: 'R' },
   ],
 }
